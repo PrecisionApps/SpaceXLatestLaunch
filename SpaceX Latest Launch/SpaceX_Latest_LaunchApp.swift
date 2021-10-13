ca13467dd06bb718892176bb5f5baf1a3e841df9
KaanOzge <kaanyildiz.iosdev@gmail.com>
2021-10-13 17:24:43 +0300

    Initial Commit

---
+++ SpaceX Latest Launch/SpaceX_Latest_LaunchApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceX_Latest_LaunchApp.swift
+//  SpaceX Latest Launch
+//
+//  Created by Kaan Yıldız on 13.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct SpaceX_Latest_LaunchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
